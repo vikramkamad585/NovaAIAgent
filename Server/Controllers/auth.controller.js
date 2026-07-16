fe99303c7dbf83d48cd6613acf9ed69398012aad
vikramkamad585 <vikram.kamad585@gmail.com>
2026-07-16 14:44:54 +0530

    Update auth.controller.js

--- Server/Controllers/auth.controller.js
+++ Server/Controllers/auth.controller.js
@@ -18,8 +18,8 @@ export const googleAuth = async (req, res) => {
         const token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -34,9 +34,9 @@ export const googleAuth = async (req, res) => {
 export const logout = async (req,res) => {
     try{
         await res.clearCookie("token", {
-            httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            httpOnly:false,
+            secure:true,
+            sameSite:"none",
         })
 
         return res.status(200).json({message:"Logout successfully"})
