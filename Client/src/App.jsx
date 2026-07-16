4965dc830b5218b82f41d520bf8f75e7dc6d2836
vikramkamad585 <vikram.kamad585@gmail.com>
2026-07-16 15:41:04 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,7 @@ import Builder from "./pages/Builder"
 import Billing from "./pages/Billing"
 import {Toaster} from 'react-hot-toast'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://novaaiagentserver.onrender.com"
 export const CLIENT_URL = "http://localhost:5173"
 
 function App(){
